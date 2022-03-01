7896345c726605852361ea1c84111e0ef8ae7ba3
alexti <alexti@outlook.com.br>
2022-03-01 15:59:27 -0300

    adding category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Observable, throwError} from 'rxjs';
+import { map, catchError, flatMap }from 'rxjs/operators';
+
+import { Category } from './category.model';
+import { url } from 'inspector';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath: string = "api/categories";
+
+  constructor(private http:HttpClient) { }
+
+  getAll(): Observable<Category[]>{
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    )
+  }
+
+  getById(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )    
+  }
+
+  create(category: Category): Observable<Category> {
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  update(category: Category): Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+    return this.http.put(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    )
+  }
+
+  private jsonDataToCategories(jsonData: any[]):Category[] {
+    const categories: Category[] = [];
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+  
+  private handleError(error: any): Observable<any> {
+    console.log("ERRO NA REQUISIÇÃO", error);
+    return throwError(error);
+  }
+
+
+  private jsonDataToCategory(jsonData: any):Category {    
+    return jsonData as Category;
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+}
